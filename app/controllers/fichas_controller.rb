8ef05375a2e44ff1dc6ff224a9a24fea22c390d1
Prelang Builder <builder@prelang.com>
2015-02-15 17:37:57 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/fichas_controller.rb
+++ app/controllers/fichas_controller.rb
@@ -1,4 +1,8 @@
 class FichasController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_ficha, only: [:show, :edit, :update, :destroy]
 
   # GET /fichas
